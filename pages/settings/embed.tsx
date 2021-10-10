a7cf16033a2b1b4a84f245b1e2a85b4310b5085e
maelgohaud <maelgohaud@gmail.com>
2021-10-10 22:56:19 +0200

    Mise à jour langue et affichage panl Embed

--- pages/settings/embed.tsx
+++ pages/settings/embed.tsx
@@ -107,19 +107,19 @@ export default function Embed(props: inferSSRProps<typeof getServerSideProps>) {
 
   return (
     <Shell
-      heading="Embed &amp; Webhooks"
-      subtitle="Integrate with your website using our embed options, or get real-time booking information using custom webhooks.">
+      heading="Intégration &amp; Webhooks"
+      subtitle="Intégrez Avocal dans votre site en utilisant nos options d'intégration, ou récupérez les informations de prise de rendez-vous en temps réel en utilisant des webhooks.">
       <SettingsShell>
         {!editWebhookEnabled && (
           <div className="py-6 lg:pb-8 lg:col-span-9">
             <div className="mb-6">
-              <h2 className="text-lg font-medium leading-6 text-gray-900 font-cal">iframe Embed</h2>
-              <p className="mt-1 text-sm text-gray-500">The easiest way to embed Cal.com on your website.</p>
+              <h2 className="text-lg font-medium leading-6 text-gray-900 font-cal">Intégrer un iframe</h2>
+              <p className="mt-1 text-sm text-gray-500">La manière la plus simple d'intégrer Avocal sur votre site internet.</p>
             </div>
             <div className="grid grid-cols-2 space-x-4">
               <div>
                 <label htmlFor="iframe" className="block text-sm font-medium text-gray-700">
-                  Standard iframe
+                  iframe standard
                 </label>
                 <div className="mt-1">
                   <textarea
@@ -133,7 +133,7 @@ export default function Embed(props: inferSSRProps<typeof getServerSideProps>) {
               </div>
               <div>
                 <label htmlFor="fullscreen" className="block text-sm font-medium text-gray-700">
-                  Responsive full screen iframe
+                  iframe responsive plein écran
                 </label>
                 <div className="mt-1">
                   <textarea
@@ -151,43 +151,43 @@ export default function Embed(props: inferSSRProps<typeof getServerSideProps>) {
               <div>
                 <h2 className="text-lg font-medium leading-6 text-gray-900 font-cal">Webhooks</h2>
                 <p className="mt-1 text-sm text-gray-500">
-                  Receive Cal meeting data at a specified URL, in real-time, when an event is scheduled or
-                  cancelled.{" "}
+                  Recevez les informations de prise de rendez-vous Avocal à un URL spécifique en temps-réel (lorsqu'un rendez-vous est pris ou
+                  annulé).{" "}
                 </p>
               </div>
               <div>
                 <Dialog>
                   <DialogTrigger className="px-4 py-2 my-6 text-sm font-medium text-white border border-transparent rounded-sm shadow-sm bg-neutral-900 hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900">
                     <PlusIcon className="inline w-5 h-5 mr-1" />
-                    New Webhook
+                    Nouveau Webhook
                   </DialogTrigger>
                   <DialogContent>
                     <DialogHeader
-                      title="Create a new webhook"
-                      subtitle="Create a new webhook to your account"
+                      title="Creer un nouveau webhook"
+                      subtitle="Créer un nouveau webhook pour votre compte"
                     />
                     <div className="my-4">
                       <div className="mb-4">
                         <label htmlFor="subUrl" className="block text-sm font-medium text-gray-700">
-                          Subscriber Url
+                          Url Cible
                         </label>
                         <input
                           ref={subUrlRef}
                           type="text"
                           name="subUrl"
                           id="subUrl"
-                          placeholder="https://example.com/sub"
+                          placeholder="https://example.com/cible"
                           required
                           className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-sm shadow-sm focus:outline-none focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
                         />
                         <legend className="block pt-4 mb-2 text-sm font-medium text-gray-700">
                           {" "}
-                          Event Triggers{" "}
+                          Événements à suivre{" "}
                         </legend>
                         <div className="p-2 border border-gray-300 rounded-sm">
                           <div className="flex pb-4">
                             <div className="w-10/12">
-                              <h2 className="font-medium text-gray-800">Booking Created</h2>
+                              <h2 className="font-medium text-gray-800">Rendez-vous créé</h2>
                             </div>
                             <div className="flex items-center justify-center w-2/12 text-right">
                               <Switch
@@ -202,7 +202,7 @@ export default function Embed(props: inferSSRProps<typeof getServerSideProps>) {
                           </div>
                           <div className="flex py-1">
                             <div className="w-10/12">
-                              <h2 className="font-medium text-gray-800">Booking Rescheduled</h2>
+                              <h2 className="font-medium text-gray-800">Rendez-vous replanifié</h2>
                             </div>
                             <div className="flex items-center justify-center w-2/12 text-right">
                               <Switch
@@ -217,7 +217,7 @@ export default function Embed(props: inferSSRProps<typeof getServerSideProps>) {
                           </div>
                           <div className="flex pt-4">
                             <div className="w-10/12">
-                              <h2 className="font-medium text-gray-800">Booking Cancelled</h2>
+                              <h2 className="font-medium text-gray-800">Rendez-vous annulé</h2>
                             </div>
                             <div className="flex items-center justify-center w-2/12 text-right">
                               <Switch
@@ -240,11 +240,11 @@ export default function Embed(props: inferSSRProps<typeof getServerSideProps>) {
                             onClick={createWebhook}
                             color="primary"
                             className="ml-2">
-                            Create Webhook
+                            Créer le Webhook
                           </Button>
                         </DialogClose>
                         <DialogClose asChild>
-                          <Button color="secondary">Cancel</Button>
+                          <Button color="secondary">Annuler</Button>
                         </DialogClose>
                       </div>
                     </div>
@@ -269,7 +269,7 @@ export default function Embed(props: inferSSRProps<typeof getServerSideProps>) {
               </div>
             </div>
 
-            <hr className="mt-8" />
+            {/* <hr className="mt-8" />
             <div className="my-6">
               <h2 className="text-lg font-medium leading-6 text-gray-900 font-cal">Cal.com API</h2>
               <p className="mt-1 text-sm text-gray-500">
@@ -278,7 +278,7 @@ export default function Embed(props: inferSSRProps<typeof getServerSideProps>) {
             </div>
             <a href="https://developer.cal.com/api" className="btn btn-primary">
               Browse our API documentation
-            </a>
+            </a> */}
           </div>
         )}
         {!!editWebhookEnabled && <EditWebhook webhook={webhookToEdit} onCloseEdit={onCloseEdit} />}
