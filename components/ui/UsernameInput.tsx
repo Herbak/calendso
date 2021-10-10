6dec0d3324292ea1bf36a4476d81cdbf9cb3200c
Herbak <maelgohaud@gmail.com>
2021-10-10 16:33:28 +0200

    test changement langue nom utilisateur

--- components/ui/UsernameInput.tsx
+++ components/ui/UsernameInput.tsx
@@ -4,7 +4,7 @@ const UsernameInput = React.forwardRef((props, ref) => (
   // todo, check if username is already taken here?
   <div>
     <label htmlFor="username" className="block text-sm font-medium text-gray-700">
-      {props.label ? props.label : "Username"}
+      {props.label ? props.label : "Nom d'utilisateur"}
     </label>
     <div className="flex mt-1 rounded-md shadow-sm">
       <span className="inline-flex items-center px-3 text-gray-500 border border-r-0 border-gray-300 rounded-l-sm bg-gray-50 sm:text-sm">
